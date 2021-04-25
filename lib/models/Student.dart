59357c9eafa270ef583fc77cede8f6042162e041
Prathamesh Mutkure <Prathamesh Mutkure>
2021-04-26 00:03:33 +0530

    Added dummy student

--- lib/models/Student.dart
+++ lib/models/Student.dart
@@ -1,13 +1,26 @@
+import 'package:anti_cheat_exam_app/models/exam/Exam.dart';
 import 'package:flutter/material.dart';
 
 class Student {
   String name;
   String id;
-  String password;
+  List<Exam> assignedExams;
 
   Student({
     @required this.id,
     @required this.name,
-    @required this.password,
+    this.assignedExams,
   });
+
+  factory Student.getDummyStudent() {
+    return Student(
+      id: '1800760300',
+      name: 'John Doe',
+      assignedExams: [
+        Exam.getDummyExam(),
+        Exam.getDummyExam(),
+        Exam.getDummyExam(),
+      ],
+    );
+  }
 }
