7e7a6a49d587f3cf81d0392074d8eea0da836dcb
Samyak Sukhdeve <samyaksukhadeve12@gmail.com>
2021-05-04 14:19:05 +0530

    Added Ui Changes

--- lib/ui/login/login_page.dart
+++ lib/ui/login/login_page.dart
@@ -13,29 +13,77 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
+        body: Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.only(left: 25.0, right: 25.0, bottom: 10.0),
+            child: TextField(
               decoration: InputDecoration(
                 hintText: "Login ID",
               ),
             ),
-            TextField(
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+            child: TextField(
               decoration: InputDecoration(
                 hintText: "Password",
               ),
             ),
-            SizedBox(height: 20),
-            TextButton(
-              onPressed: () => _loginPresses(context),
-              child: Text("Login"),
-            )
-          ],
-        ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          GestureDetector(
+            onTap: () => _loginPresses(context),
+            child: Container(
+              height: 50,
+              width: 300,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade300,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Center(
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.grey.shade900,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-    );
+    ));
   }
 }
+
+// Container(
+// padding: EdgeInsets.symmetric(horizontal: 10),
+// child: Column(
+// mainAxisAlignment: MainAxisAlignment.center,
+// children: [
+// TextField(
+// decoration: InputDecoration(
+// hintText: "Login ID",
+// ),
+// ),
+// TextField(
+// decoration: InputDecoration(
+// hintText: "Password",
+// ),
+// ),
+// SizedBox(height: 20),
+// TextButton(
+// onPressed: () => _loginPresses(context),
+// child: Text("Login"),
+// )
+// ],
+// ),
+// ),
