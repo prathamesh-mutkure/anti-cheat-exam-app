d83d305cea0d3345f586f56f697c22ef0c8fafd4
Prathamesh Mutkure <Prathamesh Mutkure>
2021-05-19 20:26:58 +0530

    Updated API base url to real backend

--- lib/data/network/api_constants.dart
+++ lib/data/network/api_constants.dart
@@ -1,5 +1,6 @@
 class Endpoints {
-  static const String baseUrl = "http://localhost:8000/api";
+  static const String baseUrl =
+      "https://anit-cheat-app-backend.herokuapp.com/api";
 
   static const String login = "$baseUrl/login";
 
