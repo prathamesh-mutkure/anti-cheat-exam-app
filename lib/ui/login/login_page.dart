ab0af085520293b0ec54dda1da8d112affe8b788
Prathamesh Mutkure <Prathamesh Mutkure>
2021-05-20 23:27:09 +0530

    Hide Password

--- lib/ui/login/login_page.dart
+++ lib/ui/login/login_page.dart
@@ -94,6 +94,7 @@ class _LoginPageState extends State<LoginPage> {
                 decoration: InputDecoration(
                   hintText: "Password",
                 ),
+                obscureText: true,
               ),
             ),
             SizedBox(
